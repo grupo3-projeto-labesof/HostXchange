2b0ab8b546945b8fac8090d2c42b8b8de7faa03c
Joice Cristina <71676439+joicecriss@users.noreply.github.com>
2024-09-16 22:08:31 -0300

    Configurando pastas

---
+++ hostXchange/src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  ngOnInit() {
+  }
+  
+}
