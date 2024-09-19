ed44ea9ac99a5fd00fe4d551a18640ee7e5d63f8
ranieri <ranieri.dev.ms@outlook.com>
2024-09-18 21:10:23 -0300

    footer e menu 1

--- hostXchange/src/app/app.component.ts
+++ hostXchange/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
