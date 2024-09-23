def43d945d44acd54e755e76f9337984691d2505
João <jgmandrade@gmail.com>
2024-09-23 20:49:58 -0300

    Juntando menu e footer na tela home.

--- hostXchange/src/app/components/menu/menu.component.ts
+++ hostXchange/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+  constructor(private router: Router) {}
+
+  login(){
+    this.router.navigate(['/login']);
+  }
 
 }
--- hostXchange/src/app/home/home.component.ts
+++ hostXchange/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../components/footer/footer.component';
+import { MenuComponent } from '../components/menu/menu.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
