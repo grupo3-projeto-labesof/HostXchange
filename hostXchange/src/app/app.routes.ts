b45a3a72e432fbce66cb18331a0a1808e8bccf95
Luis <37131912+luiscgomes1@users.noreply.github.com>
2024-09-29 21:23:49 -0300

    T7-31: FrontEnd - Tornar-se Host
    Criação da tela seguindo os requisitos da sprint 3, no Jira. :smile_cat:

--- hostXchange/src/app/app.routes.ts
+++ hostXchange/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { Host } from '@angular/core';
+import { FormHostComponent } from './form-host/form-host.component';
 
 export const routes: Routes = [
       { path: 'home' , component: HomeComponent  }
     , { path: 'login', component: LoginComponent },
+    {path: 'tornar-host', component: FormHostComponent }
 ];
---
+++ hostXchange/src/app/form-host/form-host.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-host',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-host.component.html',
+  styleUrl: './form-host.component.css'
+})
+export class FormHostComponent {
+
+}
