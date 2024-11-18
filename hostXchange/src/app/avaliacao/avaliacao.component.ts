99c537a781a87f3d89906ef6b56e576c52a9b64b
Luis <37131912+luiscgomes1@users.noreply.github.com>
2024-11-18 20:55:31 -0300

    ajustes
    :smile_cat:

---
+++ hostXchange/src/app/avaliacao/avaliacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avaliacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './avaliacao.component.html',
+  styleUrl: './avaliacao.component.css'
+})
+export class AvaliacaoComponent {
+
+}
